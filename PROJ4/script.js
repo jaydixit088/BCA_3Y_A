ade2d99d1718166fd30d92de161e5b190475c773
jaydixit088 <jay.dixit_bca22@gla.ac.in>
2024-08-16 11:47:35 +0530

    Day Night Changes

---
+++ PROJ4/script.js
@@ -0,0 +1,10 @@
+let mode=document.getElementById("mode")
+
+function day(){
+    mode.style.background="#d2f8f8"
+    mode.style.color="black"
+}
+function night(){
+    mode.style.background="#444442"
+    mode.style.color="white"
+}
